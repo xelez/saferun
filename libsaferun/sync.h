dc4d1d1ad2a535d55edd98498f79b91bfb9a01d3
Alexander Ankudinov <xelez0@gmail.com>
2012-02-01 21:01:16 +0600

    Add license blocks to all source files

--- libsaferun/sync.h
+++ libsaferun/sync.h
@@ -1,3 +1,19 @@
+/*
+ *  Copyright 2012 Alexander Ankudinov
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
 #ifndef _SYNC_H
 #define _SYNC_H
 
@@ -6,6 +22,5 @@ void sync_free(int sv[2]);
 
 int  sync_wait(int fd);
 void sync_wake(int fd, int sequence);
-
 
 #endif /*_SYNC_H*/
